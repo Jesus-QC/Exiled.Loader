3409f047fe683077dae0e7c86bb5fc42416edae8
Jesús <69375249+Jesus-QC@users.noreply.github.com>
2022-05-12 18:08:29 +0200

    💣

---
+++ Exiled.Loader/Config.cs
@@ -0,0 +1,10 @@
+﻿using PluginAPI.Core.Interfaces;
+
+namespace Exiled.Loader
+{
+    public class Config : IPluginConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool ShowDebugMessages { get; set; } = false;
+    }
+}
---
+++ Exiled.Loader/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using PluginAPI.Core;
+using PluginAPI.Loader;
+
+namespace Exiled.Loader
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name { get; } = "Exiled.Loader";
+        public override string Author { get; } = "Jesus-QC";
+        public override Version Version { get; } = new Version(1, 0, 0);
+
+        public override void OnEnabled()
+        {
+            string rootPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "EXILED");
+            
+            if (!AssemblyLoader.TryGetAssembly(Path.Combine(rootPath, "Exiled.Loader.dll"), out var loader) || !AssemblyLoader.TryGetAssembly(Path.Combine(rootPath, "Plugins", "dependencies", "Exiled.API.dll"), out var api) || !AssemblyLoader.TryGetAssembly(Path.Combine(rootPath, "Plugins", "dependencies", "YamlDotNet.dll"), out var yaml))
+            {
+                Log.Warning("Exiled was not found. Skipping.");
+                return;
+            }
+            
+            loader.GetType("Exiled.Loader.Loader").GetMethod("Run")?.Invoke(null, new object[] { new[] {api, yaml} });
+
+            base.OnEnabled();
+        }
+    }
+}
